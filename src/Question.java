fa8c6a30fd039de7c6e42b907fc462d388a88b98
Hana Kubincova <hana.kubincova@prusa3d.cz>
2024-04-12 17:46:27 +0200

    add basic object-oriented structure of quiz

---
+++ src/Question.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Question {
+    private String text;
+    private List<Answer> answers;
+    private boolean isMultiChoice;
+
+    public Question(String text, List<Answer> answers, boolean isMultiChoice) {
+        this.text = text;
+        this.answers = answers;
+        this.isMultiChoice = isMultiChoice;
+    }
+
+    public void display() {
+        System.out.println();
+        System.out.println(this.text + " " + (this.isMultiChoice ? "(Question with multiple answers)" : "(Question with single answer)"));
+
+        for (int i = 0; i < this.answers.size(); i++) {
+            System.out.println((i + 1) + ". " + this.answers.get(i).getText());
+        }
+    }
+
+    public void revealAnswer() {
+        System.out.print("Correct answer: ");
+        for (int i = 0; i < this.answers.size(); i++) {
+            if (this.answers.get(i).isCorrect()) {
+                System.out.print(i + 1);
+            }
+        }
+        System.out.println();
+    }
+
+    public boolean validateAnswer(String answer) {
+        boolean isValid = true;
+        String[] userAnswerIndexes = answer.split("");
+
+        for (int i = 0; i < userAnswerIndexes.length; i++) {
+            try {
+                int index = Integer.parseInt(userAnswerIndexes[i]) - 1;
+                if (!this.answers.get(index).isCorrect()) {
+                    isValid = false;
+                    break;
+                }
+            } catch (Exception e) {
+                isValid = false;
+                System.out.println("Invalid input.");
+            }
+        }
+        return isValid;
+    }
+}
---
+++ src/Quiz.java
@@ -0,0 +1,59 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Quiz {
+    private List<Question> questions;
+    private int totalPoints;
+
+    public Quiz(List<Question> questions) {
+        this.questions = questions;
+        this.totalPoints = 0;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        for (Question question : questions) {
+            question.display();
+            System.out.print("Your answer: ");
+            String answer = scanner.nextLine().trim();
+            if(question.validateAnswer(answer)) {
+                this.countCorrectAnswer();
+            }
+            question.revealAnswer();
+        }
+        System.out.println("Quiz ended. You had: " + this.totalPoints + "/" + this.questions.size() + " answers correct");
+    }
+
+    private void countCorrectAnswer() {
+        this.totalPoints++;
+    }
+
+    public static void main(String[] args) {
+        List<Answer> qOneAnswers = List.of(new Answer("Paris", true),
+                new Answer("Berlin", false),
+                new Answer("Rome", false),
+                new Answer("Madrid", false));
+        Question one = new Question("What is the capital of France?", qOneAnswers, false);
+
+        List<Answer> qTwoAnswers = List.of(new Answer("2", true),
+                new Answer("3", true),
+                new Answer("4", false),
+                new Answer("5", true),
+                new Answer("6", false));
+        Question two = new Question("Which of the following are prime numbers?", qTwoAnswers, true);
+
+        List<Answer> qThreeAnswers = List.of(new Answer("Jupiter", true),
+                new Answer("Mars", false),
+                new Answer("Earth", false),
+                new Answer("Saturn", false));
+        Question three = new Question("What is the largest planet in our solar system?", qThreeAnswers, false);
+
+        List<Question> questions = List.of(one, two, three);
+
+        System.out.println("Hello, welcome to Java quiz!");
+        System.out.println("The quiz has 3 questions with one or more correct answers.");
+
+        Quiz quiz = new Quiz(questions);
+        quiz.start();
+    }
+}
